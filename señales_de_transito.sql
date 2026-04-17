fa38f515ec359f08f618a9a6c9aec39d3de0fcab
Ezeyoshi_189 <josias.gimenez.423@alu.tecnica29de6.edu.ar>
2026-04-17 19:29:42 -0300

    señales de transito

--- señales_de_transito.sql
+++ señales_de_transito.sql
@@ -2,13 +2,13 @@
 CREATE DATABASE IF NOT EXISTS senales_transito;
 USE senales_transito;
 
--- Tabla de categorías 
+-- Tabla de categorias 
 CREATE TABLE categorias (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL UNIQUE
 );
  
--- Tabla de señales de tránsito
+-- Tabla de señales de transito
 CREATE TABLE senales (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
@@ -22,13 +22,13 @@ CREATE TABLE senales (
 );
 
 
--- Insertar categorías (agregar mas categorias)
+-- Insertar categorias (agregar mas categorias)
 INSERT INTO categorias (nombre) VALUES
 ('Preventiva'),
 ('Reglamentaria'),
 ('Informativa');
 
--- Insertar señales (agregar mas señales de trancito y buscar imagenes porfavor :D )
+-- Insertar señales (agregar mas señales de transito y buscar imagenes porfavor :D )
 INSERT INTO senales (nombre, descripcion, categoria_id, imagen) VALUES
 ('Curva peligrosa', 'Indica una curva pronunciada en la vía.', 1, 'imagenes/curva_peligrosa'),
 ('Pare', 'Indica que el conductor debe detenerse completamente.', 2, 'imagenes/pare'),
